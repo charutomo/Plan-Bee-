baf9019affab98378a6627f08b81521d205f10a8
Charissa <charissa.utomo@gmail.com>
2022-07-25 16:32:51 +0800

    collects 100 data points per second for current and voltage and displays

--- for ESP32/intermediate/samplingtest/samplingtest.ino
+++ for ESP32/intermediate/samplingtest/samplingtest.ino
@@ -1,5 +1,14 @@
+#include "SPI.h"
+#include "Adafruit_ILI9341.h"
 #include <Wire.h>
 #include <Beastdevices_INA3221.h>
+
+#define TFT_CLK 32
+#define TFT_MOSI 33
+#define TFT_MISO 12
+#define TFT_CS 27
+#define TFT_DC 26
+#define TFT_RST 25
 
 #define SERIAL_SPEED      115200    // serial baud rate
 #define PRINT_DEC_POINTS  3         // decimal points to print
@@ -7,8 +16,10 @@
 // Set I2C address to 0x41 (A0 pin -> VCC)
 Beastdevices_INA3221 ina3221(INA3221_ADDR40_GND);
 
-float currentarray1[20];
-float voltagearray1[20];
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+
+float currentarray1[100];
+float voltagearray1[100];
 int numloop = 0;
 
 void setup() {
@@ -17,7 +28,8 @@ void setup() {
   while (!Serial) {
       delay(1);
   }
-
+  tft.begin();
+  tft.fillScreen(ILI9341_WHITE);
   ina3221.begin();
   ina3221.reset();
 
@@ -26,7 +38,7 @@ void setup() {
 }
 
 void loop() {
-  if (numloop<20){
+  if (numloop<100){
   float current[3];
   float voltage[3];
 
@@ -62,15 +74,14 @@ void loop() {
   Serial.println();*/
 
   Serial.print("Current Array:");
-  for (int i = 0; i < 20; i = i + 1) {
+  for (int i = 0; i < 100; i = i + 1) {
     Serial.print(currentarray1[i]);
     Serial.print(", ");
   }
-
   
   Serial.println();
   Serial.print("Voltage Array:");
-  for (int i = 0; i < 20; i = i + 1) {
+  for (int i = 0; i < 100; i = i + 1) {
     Serial.print(voltagearray1[i]);
     Serial.print(", ");
   }
@@ -78,16 +89,24 @@ void loop() {
   numloop+=1;
   delay(10);
   }
-  else if(numloop == 20){
+  else if(numloop == 100){
+    tft.fillScreen(ILI9341_WHITE);
+    tft.setRotation(0);
+    tft.setCursor(40,40);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_BLACK);
     Serial.print("Average Current:");
+    tft.print("Average Current:");
     avgvalue(currentarray1);
+    tft.setCursor(40,70);
     Serial.print("Average Voltage:");
+    tft.print("Average Voltage:");
     avgvalue(voltagearray1);
     numloop=0;
-    for (int i=0; i<20; i++){
+    for (int i=0; i<100; i++){
       currentarray1[i]=0;
       voltagearray1[i]=0;
-    delay(500);
+    delay(100);
     Serial.println("New Loop");
   }
   
@@ -95,12 +114,13 @@ void loop() {
   
 }
 
-void avgvalue(float arr1[20]){
+void avgvalue(float arr1[100]){
   float totalval = 0;
-  for (int i = 0; i < 20; i++){
+  for (int i = 0; i < 100; i++){
     totalval += arr1[i];
   }
-  float avgval = totalval/20;
+  float avgval = totalval/100;
   Serial.println(avgval);
   Serial.println();
+  tft.println(avgval);
 }
