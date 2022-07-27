2af963de44853c3a56fbf8a8a43e257c3cc369f1
Charissa <charissa.utomo@gmail.com>
2022-07-27 22:23:59 +0800

    Menu refreshing for testing tmr and documentation for each function
    Menu refreshing for testing tmr and documentation for each function and tidy parts of the code to be neater

--- for ESP32/for ILI9341/ILI9341_INA226/ILI9341_INA226_Menu/ILI9341_INA226_Menu.ino
+++ for ESP32/for ILI9341/ILI9341_INA226/ILI9341_INA226_Menu/ILI9341_INA226_Menu.ino
@@ -8,61 +8,39 @@
 #include <SPI.h>
 #include <DallasTemperature.h>
 
-/*For connecting ESP32 to INA226:
- VCC --> 3V3
- GND --> GND
- SDA --> PIN 21
- SCL --> PIN 22
- ALE --> PIN 18 
- VBUS and IN- --> negative terminal / VCC of load
- IN+ --> Positive terminal of power supply
- Negative terminal of power supply --> GND of ESP32 and load */
- 
-/* For ILI9341: 
- VCC --> 3v3
- GND --> GND 
- LED --> 3v3 */
 #define TFT_CLK    32 
 #define TFT_MOSI   33
 #define TFT_MISO   12 
 #define TFT_CS     27 
 #define TFT_DC     26 
 #define TFT_RST    25 
-
 #define PRINT_DEC_POINTS  3  
+#define SENSOR_PIN  0 
+#define ROTARY_ENCODER_A_PIN 14
+#define ROTARY_ENCODER_B_PIN 17
+#define ROTARY_ENCODER_BUTTON_PIN 16
+#define ROTARY_ENCODER_VCC_PIN - 1 
+#define ROTARY_ENCODER_STEPS 4
 
-#define SENSOR_PIN  0 // ESP32 pin GPIO0 connected to DS18B20 sensor's DQ pin
-
-OneWire oneWire(SENSOR_PIN);
-DallasTemperature DS18B20(&oneWire);
-
-float tempC; // temperature in Celsius
-float tempF; // temperature in Fahrenheit
-
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-//If using the breakout, change pins as desired
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
-
-//for INA226
-INA226_WE ina226batt = INA226_WE(0x40); //battery channel
+float tempC; 
+float tempF;
 float shuntVoltage_mV = 0.0;
 float loadVoltage_V = 0.0;
 float busVoltage_V = 0.0;
 float current_mA = 0.0;
 float power_mW = 0.0; 
-
-//encoder
-#define ROTARY_ENCODER_A_PIN 14
-#define ROTARY_ENCODER_B_PIN 17
-#define ROTARY_ENCODER_BUTTON_PIN 16
-#define ROTARY_ENCODER_VCC_PIN - 1 /* 27 put -1 of Rotary encoder Vcc is connected directly to 3,3V; else you can use declared output pin for powering rotary encoder */
-#define ROTARY_ENCODER_STEPS 4
-
 int menu_state = 0;
 int value = 0;
+int batt = 1;
+int temp = 1;
+int cred = 1;
+int tech = 1;
 
-//instead of changing here, rather change numbers above
+OneWire oneWire(SENSOR_PIN);
+DallasTemperature DS18B20(&oneWire);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
+INA226_WE ina226batt = INA226_WE(0x40); 
 
 void IRAM_ATTR readEncoderISR() {
   rotaryEncoder.readEncoder_ISR();
@@ -76,15 +54,7 @@ void setup() {
   //in this example we will set possible values between 0 and 1000;
   bool circleValues = false;
   rotaryEncoder.setBoundaries(0, 1000, circleValues); //minValue, maxValue, circleValues true|false (when max go to min and vice versa)
-
-  /*Rotary acceleration introduced 25.2.2021.
-   * in case range to select is huge, for example - select a value between 0 and 1000 and we want 785
-   * without accelerateion you need long time to get to that number
-   * Using acceleration, faster you turn, faster will the value raise.
-   * For fine tuning slow down.
-   */
-  //rotaryEncoder.disableAcceleration(); //acceleration is now enabled by default - disable if you dont need it
-  rotaryEncoder.setAcceleration(250); //or set the value - larger number = more accelearation; 0 or 1 means disabled acceleration
+  rotaryEncoder.setAcceleration(250); 
   
   tft.begin();
   DS18B20.begin();
@@ -93,58 +63,12 @@ void setup() {
   tft.setRotation(0);
   tft.drawRGBBitmap(0, 0, Plan_Bee_Logo_ALIVE, BEE_WIDTH,BEE_HEIGHT);
   delay(1000);
-  
   ina226batt.init();
-  
-  /* Set Number of measurements for shunt and bus voltage which shall be averaged
-  * Mode *     * Number of samples *
-  AVERAGE_1            1 (default)
-  AVERAGE_4            4
-  AVERAGE_16           8
-  AVERAGE_64          64
-  AVERAGE_128        128
-  AVERAGE_256        256
-  AVERAGE_512        512
-  AVERAGE_1024      1024
-  */
-  ina226batt.setAverage(AVERAGE_128); // choose mode and uncomment for change of default
-  
-  /* Set conversion time in microseconds
-     One set of shunt and bus voltage conversion will take: 
-     number of samples to be averaged x conversion time x 2
-     
-     * Mode *         * conversion time *
-     CONV_TIME_140          140 µs
-     CONV_TIME_204          204 µs
-     CONV_TIME_332          332 µs
-     CONV_TIME_588          588 µs
-     CONV_TIME_1100         1.1 ms (default)
-     CONV_TIME_2116       2.116 ms
-     CONV_TIME_4156       4.156 ms
-     CONV_TIME_8244       8.244 ms  
-  */
-  ina226batt.setConversionTime(CONV_TIME_8244); //choose conversion time and uncomment for change of default
-  
-  /* Set measure mode
-  POWER_DOWN - INA226 switched off
-  TRIGGERED  - measurement on demand
-  CONTINUOUS  - continuous measurements (default)
-  */
-  ina226batt.setMeasureMode(TRIGGERED); // choose mode and uncomment for change of default
-  
-  /* Set Current Range
-    * Mode *   * Max Current *
-     MA_400          400 mA
-     MA_800          800 mA (default)
-  */
-  ina226batt.setCurrentRange(MA_800); // choose gain and uncomment for change of default
-  
-  /* If the current values delivered by the INA226 differ by a constant factor
-     from values obtained with calibrated equipment you can define a correction factor.
-     Correction factor = current delivered from calibrated equipment / current delivered by INA226
-  */
+  ina226batt.setAverage(AVERAGE_128); 
+  ina226batt.setConversionTime(CONV_TIME_8244); 
+  ina226batt.setMeasureMode(TRIGGERED); 
+  ina226batt.setCurrentRange(MA_800); 
   ina226batt.setCorrectionFactor(0.72); 
-
   menu();
 }
 
@@ -153,13 +77,17 @@ void loop() {
     value = rotaryEncoder.readEncoder();
     if (value % 4 == 0) {
       menu_state = 1;
+      batt = 1;
     } else if (value % 4 == 1) {
       menu_state = 2;
+      temp = 1;
     } else if (value % 4 == 2) {
       menu_state = 3;
+      cred = 1;
     }
     else if (value % 4 == 3) {
       menu_state = 4;
+      tech = 1;
     }
     changemenu();
   }
@@ -190,10 +118,12 @@ void changemenu() {
   1: main menu with selector on Battery Info
   2: main menu with selector on Temperature
   3: main menu with selector on Credits
+  4: main menu with selector on Tech Support
 
   7: Battery Info Page
   8: Temperature Page 
   9: Credits Page
+  10: Tech Support Page
   */
   switch (menu_state) {
   case 0:
@@ -240,7 +170,6 @@ void menu() {
   tft.println("Credits");
   tft.setCursor(70, 130);
   tft.println("Tech Support");
- 
 }
 
 // menu with selector on 1
@@ -274,21 +203,25 @@ void menu4(){
 
 // show the battery value
 void showbattery(void) {
-  tft.fillScreen(ILI9341_BLACK);
+  if (batt ==1){
+    refreshorig();
+    batt = 0;
+  }
+  else {
+    refreshbatt();
+  }
   float current[2]; //no. of channels
   float voltage[2];
-
   ina226batt.startSingleMeasurement();
   ina226batt.readAndClearFlags();
   current[0] = ina226batt.getCurrent_mA();
   voltage[0] = ina226batt.getBusVoltage_V();
   float power0 = ina226batt.getBusPower();
   float battpercent = ((voltage[0]-3.4)/(4.3-3.4))*100;
- 
   offsettext(0,3);
   tft.println("Battery");
   offsettext(40,2);
-   tft.print("Current:");
+  tft.print("Current:");
   tft.print(current[0], PRINT_DEC_POINTS);
   tft.println("mA, ");
   offsettext(70,2);
@@ -309,6 +242,13 @@ void showbattery(void) {
 
 // show the temperature
 void showtemp(void) {
+  if (temp ==1){
+    refreshorig();
+    temp = 0;
+  }
+  else{
+    refreshtemp();
+  }
   tft.fillScreen(ILI9341_BLACK);
   offsettext(0,3);
   tft.println("Temperature");
@@ -330,6 +270,10 @@ void showtemp(void) {
 
 // show the credits
 void credits(void) {
+  if (cred == 1){
+    refreshorig();
+    cred = 0;
+  }
   tft.fillScreen(ILI9341_BLACK);
   offsettext(0,3);
   tft.println("Credits");
@@ -349,7 +293,12 @@ void credits(void) {
   delay(1000);
 }
 
+//show tech support information
 void techsupport(void){
+  if (tech == 1){
+    refreshorig();
+    tech = 0;
+  }
   tft.fillScreen(ILI9341_BLACK);
   offsettext(0,3);
   tft.println("Tech Support");
@@ -362,14 +311,61 @@ void techsupport(void){
 }
 
 void offsettext(int y, int font){
+  /*
+   * Function: offsetdegree
+   * ----------------
+   * Set cursor to (40,y) which indent the screen and set text size accordingly to the input font
+   * 
+   * y: y-axis value to be input for printing text
+   * font: font size of the text to be printed on display
+   */
   tft.setCursor(40, y);
   tft.setTextColor(ILI9341_YELLOW);
   tft.setTextSize(font);
 }
 
 void offsetdegrees(int y){
+  /*
+   * Function: offsetdegree
+   * ----------------
+   * Set cursor and indent the screen to show the degree symbol
+   * 
+   * y: y-axis value to be input for the degrees symbol
+   */
   tft.setCursor(100, y);
   tft.setTextColor(ILI9341_YELLOW);
   tft.setTextSize(1);
   tft.println("o");
+}
+
+void refreshorig(void){
+  /*
+   * Function: refreshtemp
+   * ----------------
+   * Fills a blackbox space to cover the menu screen for refreshing page 
+   */
+  tft.fillRect(40,40,200,120,ILI9341_BLACK);
+}
+
+void refreshbatt(void){
+  /*
+   * Function: refreshtemp
+   * ----------------
+   * Fills a blackbox space to cover the old battery values
+   * for refreshing page on battery screen before showing new values
+   */
+  tft.fillRect(135,40,80,50,ILI9341_BLACK);
+  tft.fillRect(110,100,80,20,ILI9341_BLACK);
+  tft.fillRect(120,130,80,20,ILI9341_BLACK);
+  tft.fillRect(40,160,50,20,ILI9341_BLACK);
+}
+
+void refreshtemp(void){
+  /*
+   * Function: refreshtemp
+   * ----------------
+   * Fills a blackbox space to cover the old temperature values for refreshing page
+   * on temperature screen before showing new values
+   */
+  tft.fillRect(40,30,60,80,ILI9341_BLACK);
 }
--- for ESP32/for ILI9341/ILI9341_INA3221/menu3221new/menu3221new.ino
+++ for ESP32/for ILI9341/ILI9341_INA3221/menu3221new/menu3221new.ino
@@ -9,47 +9,38 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-
 #define TFT_CLK 32
 #define TFT_MOSI 33
 #define TFT_MISO 12
 #define TFT_CS 27
 #define TFT_DC 26
 #define TFT_RST 25
-
 #define PRINT_DEC_POINTS  3  
-
 #define SENSOR_PIN  0
-
 #define ROTARY_ENCODER_A_PIN 14
 #define ROTARY_ENCODER_B_PIN 17
 #define ROTARY_ENCODER_BUTTON_PIN 16
-#define ROTARY_ENCODER_VCC_PIN - 1 /* 27 put -1 of Rotary encoder Vcc is connected directly to 3,3V; else you can use declared output pin for powering rotary encoder */
-
+#define ROTARY_ENCODER_VCC_PIN - 1 
 #define ROTARY_ENCODER_STEPS 4
 
 int menu_state = 0;
 int value = 0;
-float currentarray1[10];
-float voltagearray1[10];
-float powerarray1[10];
-int numloop = 0;
 int batt = 1;
 int temp = 1;
 int cred = 1;
 int tech = 1;
+float tempC; 
+float tempF;
+float currentarray1[10];
+float voltagearray1[10];
+float powerarray1[10];
+int numloop = 0;
 
 Beastdevices_INA3221 ina3221(INA3221_ADDR40_GND); 
 
 OneWire oneWire(SENSOR_PIN);
 DallasTemperature DS18B20(&oneWire);
-
-float tempC; // temperature in Celsius
-float tempF; // temperature in Fahrenheit
-
-//instead of changing here, rather change numbers above
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
-
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
 void IRAM_ATTR readEncoderISR() {
@@ -57,33 +48,24 @@ void IRAM_ATTR readEncoderISR() {
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-
   //we must initialize rotary encoder
   rotaryEncoder.begin();
   rotaryEncoder.setup(readEncoderISR);
   //set boundaries and if values should cycle or not
   //in this example we will set possible values between 0 and 1000;
   bool circleValues = false;
-  rotaryEncoder.setBoundaries(0, 1000, circleValues); //minValue, maxValue, circleValues true|false (when max go to min and vice versa)
+  rotaryEncoder.setBoundaries(0, 1000, circleValues); 
+  rotaryEncoder.setAcceleration(250); 
 
-  /*Rotary acceleration introduced 25.2.2021.
-   * in case range to select is huge, for example - select a value between 0 and 1000 and we want 785
-   * without accelerateion you need long time to get to that number
-   * Using acceleration, faster you turn, faster will the value raise.
-   * For fine tuning slow down.
-   */
-  //rotaryEncoder.disableAcceleration(); //acceleration is now enabled by default - disable if you dont need it
-  rotaryEncoder.setAcceleration(250); //or set the value - larger number = more accelearation; 0 or 1 means disabled acceleration
-
-  // initalise the screen
+  // initialise the screen
   tft.begin();
   DS18B20.begin();
   tft.fillScreen(ILI9341_WHITE);
   tft.setRotation(0);
   tft.drawRGBBitmap(0, 0, Plan_Bee_Logo_ALIVE, BEE_WIDTH,BEE_HEIGHT);
   
+  // initialise INA3221
   ina3221.begin();
   ina3221.reset();
   ina3221.setShuntRes(100, 100, 100);
@@ -137,10 +119,12 @@ void changemenu() {
   1: main menu with selector on Battery Info
   2: main menu with selector on Temperature
   3: main menu with selector on Credits
+  4: main menu with selector on Tech Support
 
   7: Battery Info Page
   8: Temperature Page 
   9: Credits Page
+  10: Tech Support Page
   */
   switch (menu_state) {
   case 0:
@@ -187,7 +171,6 @@ void menu() {
   tft.println("Credits");
   tft.setCursor(70, 130);
   tft.println("Tech Support");
- 
 }
 
 // menu with selector on 1
@@ -237,31 +220,12 @@ void showbattery(void) {
   voltage[0] = ina3221.getVoltage(INA3221_CH1);
   voltagearray1[numloop] = ina3221.getVoltage(INA3221_CH1);
 
-  /*current[1] = ina3221.getCurrent(INA3221_CH2);
-  voltage[1] = ina3221.getVoltage(INA3221_CH2);
-
-  current[2] = ina3221.getCurrent(INA3221_CH3);
-  voltage[2] = ina3221.getVoltage(INA3221_CH3);*/
-
   Serial.print("Channel 1: ");
   Serial.print(current[0], PRINT_DEC_POINTS);
   Serial.print("A, ");
   Serial.print(voltage[0], PRINT_DEC_POINTS);
   Serial.println("V");
   Serial.println();
-
-  /*Serial.print("Channel 2: ");
-  Serial.print(current[1], PRINT_DEC_POINTS);
-  Serial.print("A, ");
-  Serial.print(voltage[1], PRINT_DEC_POINTS);
-  Serial.println("V");
-
-  Serial.print("Channel 3: ");
-  Serial.print(current[2], PRINT_DEC_POINTS);
-  Serial.print("A, ");
-  Serial.print(voltage[2], PRINT_DEC_POINTS);
-  Serial.println("V");
-  Serial.println();*/
 
   Serial.print("Current Array:");
   for (int i = 0; i < 10; i = i + 1) {
@@ -319,7 +283,6 @@ void showbattery(void) {
     delay(1000);
     Serial.println("New Loop");
   }
-  
   }
 }
 
@@ -370,6 +333,7 @@ void credits(void) {
   tft.println("and Qi Jie");
 }
 
+//show tech support information
 void techsupport(void){
   if (tech == 1){
     refreshorig();
@@ -384,12 +348,27 @@ void techsupport(void){
 }
 
 void offsettext(int y, int font){
+  /*
+   * Function: offsetdegree
+   * ----------------
+   * Set cursor to (40,y) which indent the screen and set text size accordingly to the input font
+   * 
+   * y: y-axis value to be input for printing text
+   * font: font size of the text to be printed on display
+   */
   tft.setCursor(40, y);
   tft.setTextColor(ILI9341_YELLOW);
   tft.setTextSize(font);
 }
 
 void offsetdegrees(int y){
+  /*
+   * Function: offsetdegree
+   * ----------------
+   * Set cursor and indent the screen to show the degree symbol
+   * 
+   * y: y-axis value to be input for the degrees symbol
+   */
   tft.setCursor(100, y);
   tft.setTextColor(ILI9341_YELLOW);
   tft.setTextSize(1);
@@ -397,10 +376,21 @@ void offsetdegrees(int y){
 }
 
 void refreshorig(void){
+  /*
+   * Function: refreshtemp
+   * ----------------
+   * Fills a blackbox space to cover the menu screen for refreshing page 
+   */
   tft.fillRect(40,40,200,120,ILI9341_BLACK);
 }
 
 void refreshbatt(void){
+  /*
+   * Function: refreshtemp
+   * ----------------
+   * Fills a blackbox space to cover the old battery values
+   * for refreshing page on battery screen before showing new values
+   */
   tft.fillRect(135,40,80,50,ILI9341_BLACK);
   tft.fillRect(110,100,80,20,ILI9341_BLACK);
   tft.fillRect(120,130,80,20,ILI9341_BLACK);
@@ -408,10 +398,28 @@ void refreshbatt(void){
 }
 
 void refreshtemp(void){
+  /*
+   * Function: refreshtemp
+   * ----------------
+   * Fills a blackbox space to cover the old temperature values for refreshing page
+   * on temperature screen before showing new values
+   */
   tft.fillRect(40,30,60,80,ILI9341_BLACK);
 }
 
 float avgvalue(float arr1[10], int numloop){
+    /*
+     * Function: avgvalue
+     * ----------------
+     * Calculate the average value of a array containing floats
+     *  avgval = sum of the numloop number of values in the array/numloop
+     *  
+     * arr1[10]: Array to be taken average
+     * numloop: The number loops that have been iterated
+     * 
+     * returns:
+     *  avgvalue 
+     */
     float totalval = 0;
     for (int i = 0; i < numloop; i++){
       totalval += arr1[i];
@@ -420,23 +428,56 @@ float avgvalue(float arr1[10], int numloop){
     return avgval;
 }
 
-float* powerarr(float carr1[10], float varr[10]){
+float* powerarr(float carr1[10], float varr1[10]){
+  /*
+   * Function: powerarr
+   * ----------------
+   * Forms a power array by multiply the current and voltage at each datapoint collected by INA3221
+   *  powerarray1[index] = currentarray[index]* voltage[index]
+   * 
+   * carr1[10]: Current Array that collected 10 datapoints from INA3221
+   * varr1[10]: Voltage Array that collected 10 datapoints from INA3221
+   * 
+   * returns:
+   * powerarray1 
+   */
   for (int i = 0; i <10; i++){
-    powerarray1[i] = carr1[i]*varr[i];
+    powerarray1[i] = carr1[i]*varr1[i];
   }
   return powerarray1;
 }
 
 
 float energy(float powerarr[10]){
+  /*
+   * Function: energy
+   * ----------------
+   * computes the energy generated through the 10 sample points collected
+   *  avgenergy = sum of 10 respective points * 0.0001 s/point
+   *  
+   *  powerarr[10]: power array collected by powerarr function
+   *  
+   *  returns:
+   *  Average Energy via Riemann Sum or Total Area under the rectangular graph at each point for Power-Time Graph
+   */
   float avgenergy = 0;
   for (int i = 0; i <10; i++){
-    avgenergy+= powerarr[i]*0.005;
+    avgenergy+= powerarr[i]*0.0001;
   }
   return avgenergy;
 }
 
 float battpercent(float avgvoltage){
+  /*  
+   *   Function: battpercent
+   *   ---------------------
+   *   compute the battery percentage in terms of voltage difference at the battery
+   *    equation are as follows battery percent = ((avgvoltage-3.4)/ (4.3 - 3.4))*100%
+   *    
+   *   avgvoltage: average voltage value after sampling taken by INA3221
+   *   
+   *   returns: batterypercent (battery percentage of power bank)
+   */
   if (avgvoltage<= 4.3 & avgvoltage >= 3.4){
     float batterypercent = ((avgvoltage-3.4)/ (4.3 - 3.4))*100;
     return batterypercent;
